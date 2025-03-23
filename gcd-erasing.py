3a04760b4431a1297f704ed9c69add32a1109b25
zusanenlask <141504442+zusanenlask@users.noreply.github.com>
2025-03-23 19:57:26 +0100

    gcd py

---
+++ gcd-erasing.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# NOTE: it is recommended to use this even if you don't understand the following code.
+
+import sys
+
+# uncomment the two following lines if you want to read/write from files
+# sys.stdin = open('input.txt')
+# sys.stdout = open('output.txt', 'w')
+
+N = int(input().strip())
+
+steps = 0
+
+
+# INSERT YOUR CODE HERE
+
+
+print(steps)
+
+sys.stdout.close()
